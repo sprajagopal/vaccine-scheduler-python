94df8e66f7cc66f4a3b77df0c5be1f67900fa819
Sree Prasanna Rajagopal <sreer@uw.edu>
2022-06-04 22:18:50 -0700

    removed environment

--- conda_env.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-conda env config vars set Server=hwsreer
-conda env config vars set DBName=hw3
-conda env config vars set UserID=sreer@hwsreer
-conda env config vars set Password=ppae221B
