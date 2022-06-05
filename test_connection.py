46f8214767a43b5b4677ae692df6de36a700f08a
Sree Prasanna Rajagopal <sreer@uw.edu>
2022-06-04 22:09:59 -0700

    modified testing code

--- test_connection.py
+++ test_connection.py
@@ -2,23 +2,7 @@ import sys
 sys.path.append('src/main/scheduler')
 from db.ConnectionManager import ConnectionManager
 import pymssql
-from model.Vaccine import Vaccine
+from model.Vaccine import get_all_available_vaccines
 
-v = Vaccine("test", 10)
-v.get()
-print(v)
-
-c = CareGiver
-
-# cm = ConnectionManager()
-# conn = cm.create_connection()
-# cursor = conn.cursor()
-# 
-# try:
-#     cursor.execute(get_all_vaccines)
-#     for row in cursor:
-#         print(f"name: {str(row['Name'])}, available_doses: {str([row['Doses']])}")
-# except pymssql.Error:
-#     print('Error occured when getting details from Vaccines')
-
+print(get_all_available_vaccines())
 
